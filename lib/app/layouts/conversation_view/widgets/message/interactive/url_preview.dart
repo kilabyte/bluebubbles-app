3ed45f18f2a9187e37b60f3f617b062c095592d2
zlshames <zlshames@gmail.com>
2026-01-01 13:19:38 -0800

    fix: small url preview improvements

--- lib/app/layouts/conversation_view/widgets/message/interactive/url_preview.dart
+++ lib/app/layouts/conversation_view/widgets/message/interactive/url_preview.dart
@@ -34,8 +34,9 @@ class UrlPreview extends StatefulWidget {
 class _UrlPreviewState extends OptimizedState<UrlPreview> with AutomaticKeepAliveClientMixin {
   UrlPreviewData get data => widget.data;
   UrlPreviewData? dataOverride;
-  dynamic get file => File(content.path!);
-  dynamic content;
+  File? get file => content is PlatformFile && content?.path != null ? File(content!.path!) : null;
+  PlatformFile? content;
+  MediaMetadata? localImageMetadata;
 
   @override
   bool get wantKeepAlive => true;
@@ -68,7 +69,7 @@ class _UrlPreviewState extends OptimizedState<UrlPreview> with AutomaticKeepAliv
               dataOverride!.imageMetadata = MediaMetadata(size: const Size.square(1), url: metadata!.image);
               dataOverride!.summary = metadata.description ?? metadata.title;
               dataOverride!.url = url;
-              setState(() {});
+              if (mounted) setState(() {});
             }
           });
         }
@@ -77,25 +78,25 @@ class _UrlPreviewState extends OptimizedState<UrlPreview> with AutomaticKeepAliv
             .firstWhereOrNull((e) => e?.transferName?.contains("pluginPayloadAttachment") ?? false);
         if (attachment != null) {
           content = AttachmentsSvc.getContent(attachment, autoDownload: true, onComplete: (file) {
-            setState(() {
-              content = file;
-            });
+            if (mounted) {
+              setState(() {
+                content = file;
+              });
+            }
           });
           if (content is PlatformFile) {
-            setState(() {});
+            if (mounted) setState(() {});
           }
         } else {
           MetadataFetch.extract((data.url ?? data.originalUrl)!).then((metadata) async {
             if (metadata?.image != null) {
-              data.imageMetadata = MediaMetadata(size: const Size.square(1), url: metadata!.image);
-              widget.message.save();
-              setState(() {});
+              localImageMetadata = MediaMetadata(size: const Size.square(1), url: metadata!.image);
+              if (mounted) setState(() {});
             } else {
               final response = await HttpSvc.dio.get((data.url ?? data.originalUrl)!);
               if (response.headers.value('content-type')?.startsWith("image/") ?? false) {
-                data.imageMetadata = MediaMetadata(size: const Size.square(1), url: (data.url ?? data.originalUrl)!);
-                widget.message.save();
-                setState(() {});
+                localImageMetadata = MediaMetadata(size: const Size.square(1), url: (data.url ?? data.originalUrl)!);
+                if (mounted) setState(() {});
               }
             }
           });
@@ -107,8 +108,9 @@ class _UrlPreviewState extends OptimizedState<UrlPreview> with AutomaticKeepAliv
   @override
   Widget build(BuildContext context) {
     super.build(context);
+    final effectiveImageMetadata = localImageMetadata ?? data.imageMetadata;
     final siteText = widget.file != null ? (dataOverride?.siteName ?? "") : Uri.tryParse(data.url ?? data.originalUrl ?? "")?.host ?? data.siteName;
-    final hasAppleImage = (data.imageMetadata?.url == null || (data.iconMetadata?.url == null && data.imageMetadata?.size == Size.zero));
+    final hasAppleImage = (effectiveImageMetadata?.url == null || (data.iconMetadata?.url == null && effectiveImageMetadata?.size == Size.zero));
     final _data = dataOverride ?? data;
     return InkWell(
       onTap: widget.file != null && _data.url != null ? () async {
@@ -121,11 +123,11 @@ class _UrlPreviewState extends OptimizedState<UrlPreview> with AutomaticKeepAliv
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (_data.imageMetadata?.url != null && ReplyScope.maybeOf(context) == null)
+          if (effectiveImageMetadata?.url != null && ReplyScope.maybeOf(context) == null)
             Container(
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: NetworkImage(_data.imageMetadata!.url!),
+                  image: NetworkImage(effectiveImageMetadata!.url!),
                   fit: BoxFit.cover,
                 ),
               ),
@@ -137,7 +139,7 @@ class _UrlPreviewState extends OptimizedState<UrlPreview> with AutomaticKeepAliv
                     child: ConstrainedBox(
                       constraints: BoxConstraints(maxHeight: context.height * 0.4),
                       child: Image.network(
-                        _data.imageMetadata!.url!,
+                        effectiveImageMetadata.url!,
                         gaplessPlayback: true,
                         filterQuality: FilterQuality.none,
                         errorBuilder: (context, object, stacktrace) => Center(
@@ -150,11 +152,11 @@ class _UrlPreviewState extends OptimizedState<UrlPreview> with AutomaticKeepAliv
                 ),
               ),
             ),
-          if (content is PlatformFile && hasAppleImage && content.bytes != null && ReplyScope.maybeOf(context) == null)
+          if (content is PlatformFile && hasAppleImage && content?.bytes != null && ReplyScope.maybeOf(context) == null)
            Container(
              decoration: BoxDecoration(
                image: DecorationImage(
-                 image: MemoryImage(content.bytes!),
+                 image: MemoryImage(content!.bytes!),
                  fit: BoxFit.cover,
                ),
              ),
@@ -165,7 +167,7 @@ class _UrlPreviewState extends OptimizedState<UrlPreview> with AutomaticKeepAliv
                   child: ConstrainedBox(
                     constraints: BoxConstraints(maxHeight: context.height * 0.4),
                     child: Image.memory(
-                      content.bytes!,
+                      content!.bytes!,
                       gaplessPlayback: true,
                       filterQuality: FilterQuality.none,
                       errorBuilder: (context, object, stacktrace) => Center(
@@ -177,11 +179,11 @@ class _UrlPreviewState extends OptimizedState<UrlPreview> with AutomaticKeepAliv
                 ),
               ),
            ),
-          if (content is PlatformFile && hasAppleImage && content.bytes == null && content.path != null && ReplyScope.maybeOf(context) == null)
+          if (content is PlatformFile && hasAppleImage && content?.bytes == null && content?.path != null && file != null && ReplyScope.maybeOf(context) == null)
             Container(
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: FileImage(file),
+                  image: FileImage(file!),
                   fit: BoxFit.cover,
                 ),
               ),
@@ -192,7 +194,7 @@ class _UrlPreviewState extends OptimizedState<UrlPreview> with AutomaticKeepAliv
                   child: ConstrainedBox(
                     constraints: BoxConstraints(maxHeight: context.height * 0.4),
                     child: Image.file(
-                      file,
+                      file!,
                       gaplessPlayback: true,
                       filterQuality: FilterQuality.none,
                       errorBuilder: (context, object, stacktrace) => Center(
